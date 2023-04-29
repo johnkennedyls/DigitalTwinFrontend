bf5fd1798bc590937fd9f05ba743ddbc29991f74
Carolina Pasuy <carolinapasuy@hotmail.com>
2023-04-28 22:11:07 -0500

    setup configuration of package.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route, Switch } from "react-router";
-import { BrowserRouter } from "react-router-dom";
-import AddAlarm from "./pages/alarm-management/AddAlarm.jsx";
-import EditAlarm from "./pages/alarm-management/EditAlarm.jsx";
-import ListAlarm from "./pages/alarm-management/ListAlarm.jsx";
+import { BrowserRouter, Switch } from "react-router-dom";
 import routes from "./routes/routes.jsx";
-import "./index.css";
-
-const router = new BrowserRouter({ routes });
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Switch>
-        <Route path="/add-alarm" component={AddAlarm} />
-        <Route path="/edit-alarm" component={EditAlarm} />
-        <Route path="/manage-alarm" component={ListAlarm} />
+        {routes()}
       </Switch>
     </BrowserRouter>
   </React.StrictMode>,
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,20 +1,15 @@
-import AddAlarm from "../pages/alarm-management/AddAlarm.jsx";
-import EditAlarm from "../pages/alarm-management/EditAlarm.jsx"; 
-import ListAlarm from "../pages/alarm-management/ListAlarm.jsx";
+import React from 'react';
+import { Route } from 'react-router';
+import AddAlarm from '../pages/alarm-management/AddAlarm';
+import EditAlarm from '../pages/alarm-management/EditAlarm';
+import ListAlarm from '../pages/alarm-management/ListAlarm';
 
-const routes = [
-  {
-    path: "/manage-alarm",
-    component: <ListAlarm />
-  },
-  {
-    path: "/edit-alarm",
-    component: <EditAlarm />
-  },
-  {
-    path: "/add-alarm",
-    component: <AddAlarm />
-  }
-];
+const routes = () => (
+  <React.Fragment>
+    <Route path="/add-alarm" component={AddAlarm} />
+    <Route path="/edit-alarm" component={EditAlarm} />
+    <Route path="/manage-alarm" component={ListAlarm} />
+  </React.Fragment>
+);
 
 export default routes;
