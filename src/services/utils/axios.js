c7b6913accbcf218c3a09c3a4a672c643e104c5b
Santiago Hurtado Solis <santiago.hurtado1@hotmail.com>
2023-05-19 21:04:49 -0500

    create axios file

---
+++ src/services/utils/axios.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL,
+    headers: {
+        "X-Requested-With": "XMLHttpRequest",
+        Accept: "application/json",
+        Authorization: {
+            toString() {
+                return `Bearer ${localStorage.getItem("access_token")}`;
+            },
+        },
+    },
+    validateStatus: function validateStatus(status) {
+        if (status === 401 && !localStorage.getItem("access_token")) {
+            window.location.href = "/";
+        }
+        return status >= 200 && status < 300;
+    },
+});
