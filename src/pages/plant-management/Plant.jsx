1d4b152e6b01ab3a061bfd75279d5184673cd070
Santiago Hurtado Solis <santiago.hurtado1@hotmail.com>
2023-06-10 10:41:14 -0500

    add circular animation

--- src/pages/plant-management/Plant.jsx
+++ src/pages/plant-management/Plant.jsx
@@ -5,12 +5,12 @@ import PlantSVG from "/src/components/utils/SVGRender";
 import { getPlantData } from "/src/services/PlantService";
 import ReactMarkdown from 'react-markdown';
 
+import CircularProgress from '@mui/material/CircularProgress';
+
 export default function Plant() {
   const { plantId } = useParams();
   const [plant, setPlant] = useState(null);
-
   const history = useHistory();
-  const basePath = import.meta.env.VITE_DASHBOARD_BASE_PATH;
 
   useEffect(() => {
     getPlantData(plantId)
@@ -19,28 +19,29 @@ export default function Plant() {
       })
       .catch((error) => {
         console.error(error);
-        // window.location.href = "/dashboard/manage-plant";
         history.push(`/manage-plant`);
       });
   }, [plantId]);
 
   return (
     <>
-      {plant === null ? <h1>Cargando datos de la planta. Esto puede tardar unos minutos</h1> :
-        <>
-
-          <Grid container>
-            <Grid item xs={12} sm={plant.conventions != '' ? 8 : 12}>
-              <PlantSVG mapSvgTag={plant.mapSvgTag} svgImage={plant.svgImage} />
-            </Grid>
-            <Grid item xs={12} sm={plant.conventions != '' ? 4 : 0} style={plant.conventions === '' ? { display: 'None' } : { display: 'initial' }}>
-              <Box overflow="auto" textAlign={'center'} >
-                <h2>Convenciones</h2>
-                <ReactMarkdown>{plant.conventions || ""}</ReactMarkdown>
-              </Box>
-            </Grid>
+      {plant === null ?
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: "5rem" }}>
+          <CircularProgress />
+          <h2>Cargando datos de la planta. Esto puede tardar unos minutos.</h2>
+        </Box>
+        :
+        <Grid container>
+          <Grid item xs={12} sm={plant.conventions != '' ? 8 : 12}>
+            <PlantSVG mapSvgTag={plant.mapSvgTag} svgImage={plant.svgImage} />
           </Grid>
-        </>
+          <Grid item xs={12} sm={plant.conventions != '' ? 4 : 0} style={plant.conventions === '' ? { display: 'None' } : { display: 'initial' }}>
+            <Box overflow="auto" textAlign={'center'} >
+              <h2>Convenciones</h2>
+              <ReactMarkdown>{plant.conventions || ""}</ReactMarkdown>
+            </Box>
+          </Grid>
+        </Grid>
       }
     </>
 
