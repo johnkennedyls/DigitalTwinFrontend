224678f834b0eb65928e842db272c4add166434d
GilmarAmezquita <1006351546@u.icesi.edu.co>
2024-03-11 13:20:32 -0500

    Bug resuelto al añadir planta.

--- src/pages/plant-management/AddPlant.jsx
+++ src/pages/plant-management/AddPlant.jsx
@@ -62,7 +62,7 @@ const AddPlant = () => {
       case 0:
         return <MainPlantForm onNext={handleNext} plantName={plant.plantName} plantDescription={plant.plantDescription} plantPhoto={plant.plantPhoto} plantIp={plant.plantIp} plantSlot={plant.plantSlot} />;
       case 1:
-        return <LoadPlantSvgForm onNext={handleNext} onBack={handleBack} svgImageUrl={plant.svgImage} conventions={plant.conventions} />;
+        return <LoadPlantSvgForm onNext={handleNext} onBack={handleBack} svgImageUrl={plant.svgImage} conventions={plant.conventions} prevMapSvgTag={plant['mapSvgTag']} />;
       case 2:
         return <TagsPlantForm onNext={handleNext} onBack={handleBack} currentTags={plant.tags} svgIds={plant['mapSvgTag']}/>;
       default:
