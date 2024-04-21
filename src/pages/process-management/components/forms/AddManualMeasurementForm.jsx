090980dd89c61569279abe68e3cdc903ef5a90c7
Rockthor <jan.carvajal1@u.icesi.edu.co>
2024-04-21 12:11:04 -0500

    The connection to the back end it workin well again

--- src/pages/process-management/components/forms/AddManualMeasurementForm.jsx
+++ src/pages/process-management/components/forms/AddManualMeasurementForm.jsx
@@ -6,6 +6,7 @@ import {
     Dialog
 } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
+import { addManualMeasurement } from "../../../../services/Api/ProcessService/";
 
 const ManualMeasurementForm = ({initialName = '', initialDescription = '' }) => {
 
